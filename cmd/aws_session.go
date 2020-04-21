f6c8cab5f9c52e95010d1bc50b0fc95fd91230b9
Eugene <jdevelop@users.noreply.github.com>
2020-04-21 12:23:22 -0400

    Added parquet schema explorer / schema dump.

--- cmd/aws_session.go
+++ cmd/aws_session.go
@@ -1,12 +1,31 @@
 package cmd
 
 import (
+	"sync"
+
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func getS3() *s3.S3 {
-	return s3.New(session.Must(session.NewSessionWithOptions(session.Options{
+var (
+	once sync.Once
+	svc  *s3.S3
+	sess *session.Session
+)
+
+func _init() {
+	sess = session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
-	})))
+	}))
+	svc = s3.New(sess)
+}
+
+func getS3() *s3.S3 {
+	once.Do(_init)
+	return svc
+}
+
+func getSession() *session.Session {
+	once.Do(_init)
+	return sess
 }
